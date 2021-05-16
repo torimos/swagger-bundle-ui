7be8b535a788546dc86ba9624b2df703d8320a50
Oleg Lytvynenko (Personal) <oleg.v.lytvynenko@gmail.com>
2021-05-16 17:21:07 +0300

    init

---
+++ specs.js
@@ -0,0 +1,4 @@
+window._specs = [
+    {url: "https://petstore.swagger.io/v2/swagger.json", name: "Petshop API"},
+    {url: "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/json/uber.json", name: "Uber API"},
+];
